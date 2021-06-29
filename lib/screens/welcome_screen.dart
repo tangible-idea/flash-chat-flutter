7a6c609fa5183c6169a4a4e16c1f7391371444cc
Mark Choi <mark.choi@tangibleidea.net>
2021-06-30 00:41:56 +0900

    Logo text animation with animated_text_kit.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = "/welcome";
@@ -27,12 +28,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                       child: Image.asset('images/logo.png'),
                       height: 60.0,
                     )),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
-                  ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Tangibly',
+                      textStyle: TextStyle(
+                        fontSize: 45.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      speed: const Duration(milliseconds: 80),
+                    )
+                  ],
+                  repeatForever: true,
                 ),
               ],
             ),
