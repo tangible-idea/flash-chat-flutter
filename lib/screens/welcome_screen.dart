2843901aca6d5bac1b8964a26c9419e6e5c317eb
Mark Choi <mark.choi@tangibleidea.net>
2021-06-30 01:44:20 +0900

    kPlaceHolderDecoration로 텍스트 스타일 재활용.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flash_chat/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = "/welcome";
@@ -62,31 +63,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               },
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  RoundedButton({this.content, this.color, @required this.onPressed});
-  final String content;
-  final Color color;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(content),
         ),
       ),
     );
