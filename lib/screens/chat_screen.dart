8a05af7b263c6c54d88b935afe5a967e25ccdc63
Mark Choi <mark.choi@tangibleidea.net>
2021-07-10 00:45:58 +0900

    메시지 텍스트 리스트뷰로 이쁘게 표시. Lesson190.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,6 +14,7 @@ class _ChatScreenState extends State<ChatScreen> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseFirestore _fs = FirebaseFirestore.instance;
 
+  final messageTextController = TextEditingController();
   String typedText = "";
 
   @override
@@ -47,15 +48,17 @@ class _ChatScreenState extends State<ChatScreen> {
                             color: Colors.lightBlueAccent));
                   }
                   final messages = snapshot.data.docs;
-                  List<Text> messageWidgets = [];
+                  List<MessageBubble> messageBubbles = [];
                   for (var message in messages) {
                     final messageText = message.get('text');
                     final messageSender = message.get('sender');
-                    final messageWidget =
-                        Text('$messageText from $messageSender');
-                    messageWidgets.add(messageWidget);
+                    final messageBubble = MessageBubble(
+                      text: messageText,
+                      sender: messageSender,
+                    );
+                    messageBubbles.add(messageBubble);
                   }
-                  return Column(children: messageWidgets);
+                  return Expanded(child: ListView(children: messageBubbles));
                 }),
             Container(
               decoration: kMessageContainerDecoration,
@@ -64,6 +67,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         //Do something with the user input.
                         typedText = value;
@@ -71,9 +75,10 @@ class _ChatScreenState extends State<ChatScreen> {
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
-                  FlatButton(
+                  TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      // 메시지 전송 버튼 클릭 시!
+                      messageTextController.clear();
                       _fs.collection("messages").add({
                         "sender": _auth.currentUser.email,
                         "text": typedText
@@ -90,6 +95,38 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({this.text, this.sender});
+  final String sender;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        Text(
+          sender,
+          style: TextStyle(color: Colors.black54, fontSize: 10.0),
+        ),
+        Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(30.0),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+              child: Text(
+                text,
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            color: Colors.lightBlueAccent),
+      ]),
     );
   }
 }
